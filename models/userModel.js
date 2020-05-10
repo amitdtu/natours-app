40bcc636a7d94913622f49c05b1f7f58e7588f5c
amitdtu <amitdtu007@gmail.com>
2020-05-10 21:52:07 +0530

    rm variables

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -14,18 +15,33 @@ const userSchema = new mongoose.Schema({
     unique: true,
     lowercase: true,
     validate: [validator.isEmail, 'please provide a valid email'],
+    required: [true, 'user must have email'],
   },
   photo: String,
   password: {
     type: String,
     required: [true, 'user must have password'],
-    minLength: [8, 'name must be greater or equal to 8'],
+    // minLength: [8, 'name must be greater or equal to 8'],
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'user must have password'],
+    required: [true, 'user must have confirm password'],
     // minLength: [8, 'name must be greater or equal to 8'],
   },
+});
+
+userSchema.pre('save', async function (next) {
+  console.log(this);
+  // only run this function if password is actually modified
+  if (!this.isModified('password')) return next();
+
+  //   encrypt password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // delete passwordConfirm field
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
