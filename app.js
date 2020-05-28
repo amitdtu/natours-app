32608799b2c0ca1c4c905467bba1e4f752ede65d
amitdtu <amitdtu007@gmail.com>
2020-05-29 00:46:22 +0530

    change path 1.5

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.use(function (req, res) {
-    res.sendFile(path.resolve(`${__dirname}/natours/client/build/index.html`));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
